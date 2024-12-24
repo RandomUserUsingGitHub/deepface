8ad572a2c22f9687d26f18e19c7dbb455afe1845
RandomUserUsingGitHub <Rmin.H81@gmail.com>
2024-12-24 11:20:31 +0330

    bruh3

--- deepface/models/face_detection/FastMtCnn.py
+++ deepface/models/face_detection/FastMtCnn.py
@@ -76,7 +76,7 @@ class FastMtCnnClient(Detector):
             ) from e
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        face_detector = fast_mtcnn(device=device, thresholds=[0.3, 0.4, 0.5], factor=0.55, min_face_size=10) 
+        face_detector = fast_mtcnn(device=device, thresholds=[0.2, 0.3, 0.4], factor=0.5, min_face_size=8) 
 
         return face_detector
 
