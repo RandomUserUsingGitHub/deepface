e4248da51315ceb4b7f8f236fd64234aa9c23767
RandomUserUsingGitHub <Rmin.H81@gmail.com>
2024-12-24 10:16:54 +0330

    lower confidence of fastmtcnn

--- deepface/models/face_detection/FastMtCnn.py
+++ deepface/models/face_detection/FastMtCnn.py
@@ -76,7 +76,7 @@ class FastMtCnnClient(Detector):
             ) from e
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        face_detector = fast_mtcnn(device=device)
+        face_detector = fast_mtcnn(device=device, thresholds=[0.5, 0.6, 0.7])
 
         return face_detector
 
